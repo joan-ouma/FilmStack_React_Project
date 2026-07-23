e56e20030035f0e6790d71eca244fa7344788150
joan ouma <joanouma48@gmail.com>
2026-07-23 22:42:01 +0300

    updated App component to include AuthProvider and new routes

--- filmstack/src/App.jsx
+++ filmstack/src/App.jsx
@@ -1,20 +1,27 @@
 import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 import { WatchlistProvider } from "./contexts/WatchlistContext";
 import Discover from "./pages/Discover";
 import Watchlist from "./pages/Watchlist";
-import NavBar from "./components/NavBar"; // Create a simple Nav with links
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <WatchlistProvider>
-      <NavBar />
-      <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Discover />} />
-          <Route path="/watchlist" element={<Watchlist />} />
-        </Routes>
-      </main>
-    </WatchlistProvider>
+    <AuthProvider>
+      <WatchlistProvider>
+        <NavBar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Discover />} />
+            <Route path="/watchlist" element={<Watchlist />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </main>
+      </WatchlistProvider>
+    </AuthProvider>
   );
 }
 
