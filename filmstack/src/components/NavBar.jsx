20bfee092a43c49738093766f5e7537d2346de9a
joan ouma <joanouma48@gmail.com>
2026-07-23 22:28:54 +0300

    updated NavBar component with branding and links

--- filmstack/src/components/NavBar.jsx
+++ filmstack/src/components/NavBar.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 function NavBar() {
     return (
         <nav className="navbar">
-            <div className="navbar-brand">
+            <div className="nav-brand">
                 <Link to="/">FilmStack</Link>
             </div>
-            <div className="navbar-links">
-                <Link to="/" className="nav-link">Discover</Link>
+            <div className="nav-links">
+                <Link to="/" className="nav-link">Home</Link>
                 <Link to="/watchlist" className="nav-link">Watchlist</Link>
             </div>
         </nav>
