0738349e7d88dd603bd39bb1c3173ef4bd055453
joan ouma <joanouma48@gmail.com>
2026-07-23 22:46:46 +0300

    mounted watchlist routes in express server entrypoint

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
+import watchlistRoutes from "./routes/watchlist.js";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/watchlist", watchlistRoutes);
 
 app.get("/", (req, res) => {
     res.send("FilmStack API is running...");
